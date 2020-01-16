01f2cef6cb8c9c359354dbec1547b5ac81bad7ce
Chris <chris.johnson1479@gmail.com>
2020-01-15 23:00:42 -0800

    Added Linked List program
    Linked list program revised and edited from my data structures class.
    Would like to explore circular linked lists within a GUI framework such
    as Qt5.

---
+++ c++/generic_code/Linked_list/main.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <cstdlib>
+#include <stdlib.h>
+//#include <time.h> used to prototype I used rand() to generate random values while building my linked list so I didn't have to keep entering values I could just have them randomly made.
+#include <iostream>
+#include "node.h"	//linking 
+#include "linked_list.h"	//linking
+
+using namespace std;
+
+int menu();
+void clearScreen();
+void pauseScreen();
+//declaring forward declarations to initialize before calling in main
+
---
+++ c++/generic_code/Linked_list/node.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <cstdlib>
+
+
+class Node
+{
+public:
+	int data = 0;	//initialized to zero	
+	Node* next = NULL; // initialized to NULL whenever an instace is generated.
+	Node(void);	//default constructor
+
+private:
+	
+	
+};
