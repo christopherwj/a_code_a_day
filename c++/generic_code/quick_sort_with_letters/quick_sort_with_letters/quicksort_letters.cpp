fdec541c5bfe71a4fc6fd87d7c6fbd15cb2d08e9
Chris Johnson <chris.johnson1479@gmail.com>
2020-01-20 23:01:09 -0800

    revisted old quicksort but with letters
    I knew that char values are stored as ASCII, the sorting algorithm would
    have no problem computing since all letter values have numerical
    represenations. For example, the letter 'A' has an ASCII value of 65,
    'B' is 66. As you can see sorting is easy since the alphabet in ASCII is
    sequenced equivalently.

---
+++ c++/generic_code/quick_sort_with_letters/quick_sort_with_letters/quicksort_letters.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+using namespace std;
+
+
+/*
+DISCLAIMER: This code is not my own work and is interpreted from:
+https://www.geeksforgeeks.org/quick-sort/
+
+I do not claim credit for the code listed below:
+*/
+
+
+
+
+void swap(char* a, char* b)
+{
+	char t = *a;
+	*a = *b;
+	*b = t;
+
+}
+
+
+char partition(char arr[], char low, char high)
+{
+	char pivot = arr[high];
+	char i = (low - 1);
+
+	for (char j = low; j <= high - 1; j++)
+	{
+		if (arr[j] < pivot)
+		{
+			i++;
+			swap(&arr[i], &arr[j]);
+		}
+	}
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
+}
+
+/* The main function that implements QuickSort
+arr[] --> Array to be sorted,
+low --> Starting index,
+high --> Ending index */
+void quickSort(char arr[], char low, char high)
+{
+	if (low < high)
+	{
+		/* pi is partitioning index, arr[p] is now
+		at right place */
+		char pi = partition(arr, low, high);
+
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
+}
+
+
+/* Function to print an array */
+void printArray(char arr[], char size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+
+// Driver Code 
+int main()
+{
+	char arr[] = { 'A','V','S','D','D','Z' };
+	char n = sizeof(arr) / sizeof(arr[0]);
+	quickSort(arr, 0, n - 1);
+	cout << "Sorted array: \n";
+	printArray(arr, n);
+	return 0;
+}
+
