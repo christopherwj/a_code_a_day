15f32a719d7c633d741c9cde5d794d6aaf9a73f1
Chris <chris.johnson1479@gmail.com>
2020-01-25 21:25:31 -0800

    determined the size of common cpp data types

---
+++ c++/generic_code/sizeof/sizeof/sizeof.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << " The size of int is:" << sizeof(int) << endl;
+	cout << " The size of float is:" << sizeof(float) << endl;
+	cout << " The size of char is:" << sizeof(char) << endl;
+	cout << " The size of bool is:" << sizeof(bool) << endl;
+	cout << " The size of short int is:" << sizeof(short int) << endl;
+	cout << " The size of long int is:" << sizeof(long int) << endl;
+	cout << " The size of signed long int is:" << sizeof(signed long int) << endl;
+	cout << " The size of unsigned long int is:" << sizeof(unsigned long int) << endl;
+	cout << " The size of double is:" << sizeof(double) << endl;
+	cout << " The size of wchar_t is:" << sizeof(wchar_t) << endl;
+}
