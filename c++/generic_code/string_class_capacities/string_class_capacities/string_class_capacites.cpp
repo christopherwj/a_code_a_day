a1f74c83546d5f62229014ff9e5ca359a093261d
Chris <chris.johnson1479@gmail.com>
2020-01-27 23:52:56 -0800

    Played around with string class
    Read some documentation on the string class available for the built in
    data type strings. I wanted to learn more about strings since my next
    project will be using them to display nutrition data in my diet program
    I am creating.

---
+++ c++/generic_code/string_class_capacities/string_class_capacities/string_class_capacites.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string str = " Hello, my name is chris.";
+	cout << str.size() << endl;
+	cout << str.capacity() << endl;
+	cout << str.length() << endl;
+	cout << str.empty() << endl;
+	cout << str << endl;
+	str.push_back('push');
+	cout << str << endl;
+	str.pop_back();
+	cout << str << endl;
+	str.resize(6);
+	cout << str << endl;
+}
