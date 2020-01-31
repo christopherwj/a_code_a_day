61a821f555c198ddaf78712ede65edb13f931d06
Chris <chris.johnson1479@gmail.com>
2020-01-30 23:45:37 -0800

    experimented with ofstream swap
    Learned how to use the swap function to switch chaining from one
    ofstream to swap it to change to another. By adding line 16 I can have
    it write to a different file despite the ofstream arg being for another.
    
    I can see this being very useful with conditionals where I need to
    output data to certain files depending on requirements. This means I can
    use simple switch statements with the ofstream swap arg to easily direct
    the flow of data depending on requirements without needs to write a
    bunch of code. Then again it may be bad practice, need to learn more
    about best practices and efficient methodologies.
    
     #Pleasi enter the commit message for your changes. Lines starting

--- c++/generic_code/basic_file_output/file_output.cpp
+++ c++/generic_code/basic_file_output/file_output.cpp
@@ -4,13 +4,20 @@
 using namespace std;
 
 int main() {
-	ofstream myfile("output.txt");
-	if (myfile.is_open())
+	ofstream fileOne("output_one.txt");
+	ofstream fileTwo("output_two.txt");
+	if (fileOne.is_open())
 	{
-		myfile << "This is an example of writing to a file." << endl;
-		myfile << "This should be the second line of the file." << endl;
-		myfile.close();
+		fileOne << "This is an example of writing to a file." << endl;
+		fileOne << "This should be the second line of the file." << endl;
 	}
 	else cout << "File cannot be openned, please try again.";
+	
+	fileOne.swap(fileTwo); // this switches "swap" ostream output to other file
+			       // Here I pass things to file two and they write to file one
+	fileTwo << "This originated in file two." << endl;
+
+	fileOne.close();
+	fileTwo.close();
 	return 0;
 }
