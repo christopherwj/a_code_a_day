20bb2023940ff7b29da2c11bd47adf8a67384e92
Chris <chris.johnson1479@gmail.com>
2020-02-03 21:13:01 -0800

    added size of double
    While it may seem like something you can just look up. I am finding the
    sizeof command giving me different numbers than online code. This could
    be because if ran with a 32bit OS then the size of data types change.
    
    Was able to find this out about doubles online:
    double is a 64 bit IEEE 754 double precision Floating Point Number (1 bit for the sign, 11 bits for the exponent, and 52* bits for the value), i.e. double has 15 decimal digits of precision.
    source:https://www.geeksforgeeks.org/difference-float-double-c-cpp/

--- c++/generic_code/sizeof/sizeof/sizeof.cpp
+++ c++/generic_code/sizeof/sizeof/sizeof.cpp
@@ -22,6 +22,7 @@ int main() {
 	cout << " The size of the chris class is:" << sizeof(Chris) << endl;
 	cout << " The size of int is:" << sizeof(int) << endl;
 	cout << " The size of float is:" << sizeof(float) << endl;
+	cout << " The size of double is:" << sizeof(double) << endl;
 	cout << " The size of char is:" << sizeof(char) << endl;
 	cout << " The size of bool is:" << sizeof(bool) << endl;
 	cout << " The size of short int is:" << sizeof(short int) << endl;
