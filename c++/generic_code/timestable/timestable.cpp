4a7dc4469eae6bcbc6efe3200373ff3f8cc0c244
Chris Johnson <chris.johnson1479@gmail.com>
2020-02-07 23:31:42 -0800

    Had fun using cmath
    Created a table that was not only a tiems table, but also aquared the
    resulting value before printing. At first I used int as a data type and
    this caused a table with -278237843 value to appear. I realized from the
    size of the number and from teh sign, that I had an overflow. So, to fix
    this I used a 'double' data type as the last data type to store before
    printing and this fixed the problem!

--- c++/generic_code/timestable/timestable.cpp
+++ c++/generic_code/timestable/timestable.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <cmath>
 
 int main() {
 	
-
+	//normal times table
 	for (int i = 1; i < 16; i++) {
 		if (i < 10) {
 			std::cout << " ";
@@ -16,4 +17,26 @@ int main() {
 		}
 		std::cout <<"  " <<std::endl;
 	}
+	std::cout << std::endl << std::endl << std::endl;
+	
+	//times tablesquared values 
+	for (int i = 1; i < 16; i++) {
+		if (i < 10) {
+			std::cout << " ";
+		}
+		for (int j = 1; j < 16; j++) {
+			if (j < 16) {
+				std::cout << "  ";
+			}
+			double a = 0;
+			a = i * j;
+			double b;
+			b = exp2(a);
+
+			std::cout << b << "   ";
+			//std::cout << std::endl;
+		}
+		std::cout << "  " << std::endl;
+	}
+
 }
