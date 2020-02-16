bf9c26465fbe7989604120da1b57c844703c234c
chris@iot <chris.johnson1479@gmail.com>
2020-02-15 18:33:22 -0800

    revamped some of the formatting
    Experimented with couting with different precisions using #include
    <limits> and cout.precision(#) in order to increase precision for output

--- c++/generic_code/typecasting/typecasting.cpp
+++ c++/generic_code/typecasting/typecasting.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <limits>
+
 
 //Typecasting is making a data structure act like another data structure for a single operation. It is typically used in such a manner where precision isn't lost.
 
@@ -7,9 +9,11 @@
 
 
 int main() {
+//std::cout.precision(10);
 
-	for (int i = 0; i < 128; i++) {
-		std::cout << i << " " << (char)i << std::endl;
+	for (int i = 1; i < 128; i++) {
+		 std::cout << "double:" << i <<"  " << "char:" << (char)i<<"  " <<  std::endl;
 	}
+	
 	return 0;
 }
