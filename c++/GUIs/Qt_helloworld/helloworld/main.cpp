7219703bfb1630cf65c7415b51fdacf2ccee2649
chris <Chris.johnson@gmail.com>
2020-02-16 21:17:57 -0800

    Added application Style window
    Created a window that pops up and now reads hello world. Will be adding more
    code like buttons and other visual objects.

--- c++/GUIs/Qt_helloworld/helloworld/main.cpp
+++ c++/GUIs/Qt_helloworld/helloworld/main.cpp
@@ -1,11 +1,11 @@
 #include <QCoreApplication>
 #include <QDebug>
+#include <QLabel>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
-
-    qDebug() << "hello World";
-
+    QLabel *Label = new Qlabel {"Hello World!"};
+    label->setWindowTitle("My App");
+    label->show();
     return a.exec();
 }
