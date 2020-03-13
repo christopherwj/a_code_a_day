0d2a5f46c736ef81995645f0726f3c9726425d90
chris <chris.johnson1479@gmail.com>
2020-03-12 23:11:19 -0700

    Changed variable naming to be more consistent

--- python/basic_web_scraping/find_me_the_best_coffee.py
+++ python/basic_web_scraping/find_me_the_best_coffee.py
@@ -6,20 +6,20 @@ import pandas as pd
 url = 'https://sandiego.eater.com/maps/new-coffee-shops-san-diego-essential'
 data = requests.get(url)
 soup = BeautifulSoup(data.text, 'html.parser')
-bars = soup.find("div")
-bar_names = bars.find_all("h1")
+coffee = soup.find("div")
+coffee_shop_names = coffee.find_all("h1")
 #print(bar_names)
 
 #clean up the output
-bar_list = []
+coffee_shop_list = []
 
-for b in bar_names[1:]:
+for b in coffee_shop_names[1:]:
 	result = b.text.strip()
-	bar_list.append(result)
+	coffee_shop_list.append(result)
 
-#print(len(bar_names))
-#print(bar_names)
-#print(bar_list)
+#print(len(coffee_shop_names))
+#print(coffee_shop_names)
+#print(coffee_shop_list)
 
 #get rid of u' that is before each number'
 #bar_list = [e[2:] for e in bar_list]
@@ -31,5 +31,5 @@ for b in bar_names[1:]:
 
 
 #print list of coffee shops
-for item in bar_list:
+for item in coffee_shop_list:
 	print item
