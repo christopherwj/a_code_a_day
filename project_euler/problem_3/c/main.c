533d09cdbdd3fdaa91af7085abc0b7dae9ba8a3c
chris <chris.johnson1479@gmail.com>
2020-04-25 03:59:04 -0700

    finished problem 4
    Much easier than 3, required a little tweaking

--- project_euler/problem_3/c/main.c
+++ project_euler/problem_3/c/main.c
@@ -2,7 +2,10 @@
 #include <math.h>
 #include <stdbool.h>
 
+//#define ll long long
+
 bool IsPrime(unsigned int number) {
+    if (number <= 1) return false; // zero and one are not prime
     unsigned int i;
     for (i=2; i*i<=number; i++) {
         if (number % i == 0) return false;
@@ -18,12 +21,13 @@ int main(void) {
 	
 	while ( i < (sqrt(number) + 1)){
 		if (IsPrime(i) == true && number % i == 0){
-			//printf("%d\n", i);
+			printf("%d\n", i);
 			largestPrime = i;
 		}
+		//printf("%d\n", i);
 		i++;
 	}
-	printf("%d\n", largestPrime);
+	printf("%d", largestPrime);
 
 	return 0;
 }
